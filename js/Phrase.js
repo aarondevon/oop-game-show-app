3370b831e745900b6451e2ba6ad8865bfccd6c0b
Aaron Sawyer <me@aarondevon.com>
2019-05-07 20:19:10 -0500

    create addPhraseToDisplay method

--- js/Phrase.js
+++ js/Phrase.js
@@ -2,4 +2,28 @@ class Phrase {
 	constructor(phrase) {
 		this.phrase = phrase.toLowerCase();
 	}
+	/**
+	* Display phrase on game board
+	*/
+	addPhraseToDisplay() {
+		// store the ul element found in the phrase div 
+		const ul = document.getElementById('phrase').getElementsByTagName('ul')[0];
+		
+		// put phrase into an array. Each index is 
+		// a separate letter  or space
+		const phraseArray = this.phrase.split('')
+		for (let i = 0; i < phraseArray.length; i++) {
+			const li = document.createElement('LI');
+			 if (phraseArray[i] === ' ') {
+				
+				li.className = 'hide letter space';
+				li.textContent = phraseArray[i];
+				ul.appendChild(li);
+			} else {
+				li.className = `hide letter ${phraseArray[i]}`;
+				li.textContent = phraseArray[i];
+				ul.appendChild(li);
+			 }
+		}
+	}
 }
